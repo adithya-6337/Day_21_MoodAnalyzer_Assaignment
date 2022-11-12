29ec727b0ad23eb690f9e0eb99a14c460d091065
adithya-6337 <veldandiadithya337@gmail.com>
2022-11-12 14:56:16 +0530

    Given I am in Any Mood messege return Happy

--- src/com/bridgelabz/MoodAnalyzer.java
+++ src/com/bridgelabz/MoodAnalyzer.java
@@ -3,17 +3,17 @@ package com.bridgelabz;
 public class MoodAnalyzer {
     public static void main(String[] args) {
         MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-        String happyMood = moodAnalyser.analyseMood("I am in Happy mood");
+        String happyMood = moodAnalyser.analyseMood("I am in Happy Mood");
         System.out.println("Mood is " + happyMood);
-        String sadMood = moodAnalyser.analyseMood("I am in Sad mood");
+        String sadMood = moodAnalyser.analyseMood("I am in Sad Mood");
         System.out.println("Mood is " + sadMood);
-
     }
 
-    String analyseMood(String message) {
-        if (message.toLowerCase().contains("sad")) {
+   public String analyseMood(String message){
+        if(message.toLowerCase().contains("sad")){
             return "SAD";
-        } else if (message.toLowerCase().contains("happy")) {
+        }
+        else if (message.toLowerCase().contains("happy")){
             return "HAPPY";
         }
         return message;
